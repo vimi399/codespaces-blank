dc603babd39b8d8ea5910ba55f3c8bcc298d6111
Lovro Šnajdar <snajdarlovro0@gmail.com>
2026-09-11 10:58:16 +0000

    first commit

---
+++ mimovrste-klici.py
@@ -0,0 +1,36 @@
+
+import requests #pip install requests
+"""
+# klic HTML strežnik
+base_url = "https://www.mimovrste.com/"
+call = requests.get(base_url)
+
+print(call)
+"""
+# API klic
+def getCurrTemp(lat, lon):
+    url =f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1"
+    call = requests.get(url).json()
+
+    print(call["current"]["temperature_2m"])
+
+getCurrTemp(46.21827172610609, 14.373366916081991)
+#najdi natoplejše mesto
+cities = [
+    ("Ljubljana", 46.0511, 14.5051),
+    ("Maribor",   46.5558, 15.6459),
+    ("Kranj",     46.2389, 14.3556),
+    ("Celje",     46.2309, 15.2604),
+    ("Koper",     45.5482, 13.7296),
+    ("Velenje",   46.3572, 15.1128),
+    ("Novo mesto", 45.8040, 15.1689),
+    ("Ptuj",      46.4201, 15.8702),
+    ("Kamnik",    46.2259, 14.6121),
+    ("Jesenice",  46.4324, 14.0623),
+]
+
+for a in cities():
+    lat = a[1]
+    lon = a[2]
+    getCurrTemp(lat,lon)
+    
